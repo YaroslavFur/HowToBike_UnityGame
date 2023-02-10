de5913fdbb9de640a00553afc4e722ef0925caf2
Yaroslav Fur <tapocki20@gmail.com>
2023-02-10 13:46:45 +0200

    Bike with Simple Wheels Test 3: decreasing shaking and wheel slip in process, added DebugGUI special asset to graph

--- Assets/Scripts/BikeScript.cs
+++ Assets/Scripts/BikeScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,8 +7,18 @@ public class BikeScript : MonoBehaviour
 {
     public GameObject frontWheel;
     public GameObject backWheel;
+    public GameObject bike;
     public float maxMotorTorque; // maximum torque the motor can apply to wheel
     public float maxSteeringAngle; // maximum steer angle the wheel can have
+
+    [DebugGUIGraph(group: 1, min: 0, max: 3, r: 0, g: 1, b: 0, autoScale: false)]
+    float wheelSlip;
+
+    [DebugGUIGraph(group: 2, min: 1, max: 1.01f, r: 1, g: 0, b: 0, autoScale: true)]
+    float wheelPositionY;
+
+    [DebugGUIGraph(group: 3, min: 0, max: 10f, r: 0, g: 0, b: 1, autoScale: true)]
+    float bikeSpeedZ;
 
     // Start is called before the first frame update
     void Start()
@@ -44,10 +55,25 @@ public class BikeScript : MonoBehaviour
         Debug.Log("FrontWheel Roatation: Z = " + frontWheel.transform.rotation.z + " Steering angle = " + steering);
 
         frontWheel.GetComponentInChildren<WheelCollider>().steerAngle = steering;
+
         backWheel.GetComponentInChildren<WheelCollider>().motorTorque = motor;
 
         ApplyLocalPositionToVisuals(frontWheel.GetComponentInChildren<WheelCollider>(), frontWheel.transform.Find("Wheel Mesh"));
         ApplyLocalPositionToVisuals(backWheel.GetComponentInChildren<WheelCollider>(), backWheel.transform.Find("Wheel Mesh"));
+
+        double wheelSpeed = backWheel.GetComponentInChildren<WheelCollider>().radius * 2 * Math.PI * backWheel.GetComponentInChildren<WheelCollider>().rpm / 60;
+        bikeSpeedZ = (float)bike.GetComponentInChildren<Rigidbody>().velocity.z;
+        wheelSlip = (float)Math.Round(wheelSpeed / bikeSpeedZ, 2);
+        wheelPositionY = backWheel.transform.position.y;
+
+        Debug.Log("BackWheel:" +
+            " Wheel speed: " + Math.Round(wheelSpeed, 2) + 
+            " Bike speed: " + Math.Round(bikeSpeedZ, 2) + 
+            " Wheel Slip: " + wheelSlip);
+
+        DebugGUI.LogPersistent("wheelSlip", (wheelSlip).ToString("F3"));
+        DebugGUI.LogPersistent("wheelPositionY", (wheelPositionY).ToString("F3"));
+        DebugGUI.LogPersistent("speed", (bikeSpeedZ).ToString("F3"));
     }
 
     // finds the corresponding visual wheel
